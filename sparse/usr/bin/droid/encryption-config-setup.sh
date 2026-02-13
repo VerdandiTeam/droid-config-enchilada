aef7f7c700952a42acc91af7f52f9625bad0dd65
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2026-02-13 22:48:40 +0100

    Make home unencrypted when updating

--- sparse/usr/bin/droid/encryption-config-setup.sh
+++ sparse/usr/bin/droid/encryption-config-setup.sh
@@ -14,3 +14,16 @@ mount=/home
 type=file
 size_mb=$IMAGE_SIZE
 EOL
+
+if [[ -f "/home/defaultuser/.jolla-startupwizard-usersession-done" || -f "/home/defaultuser/.config/jolla-startupwizard-usersession-done" || -f "/home/nemo/.jolla-startupwizard-usersession-done" || -f "/home/nemo/.config/jolla-startupwizard-usersession-done" ]]; then
+cat >/etc/sailfish-device-encryption-community/config.ini <<EOL
+[home_device]
+device=/home.img
+mapper=home_encrypted
+mount=/home
+name=Home
+size_mb=$IMAGE_SIZE
+state=plain
+type=file
+EOL
+fi
